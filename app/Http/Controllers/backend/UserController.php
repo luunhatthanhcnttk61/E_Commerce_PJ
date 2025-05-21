e845f04fbe64002badaf9da0deb85784892b77d6
luunhatthanhcnttk61 <6151071101@st.utc2.edu.vn>
2025-05-21 21:24:50 +0700

    update pagination

--- app/Http/Controllers/backend/UserController.php
+++ app/Http/Controllers/backend/UserController.php
@@ -4,22 +4,31 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Services\Interfaces\UserServiceInterface as UserService;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
 
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
     }
     public function index()
     {
+        $users = $this->userService->paginate();
 
+        /*phân trang cho user */
+        // $users = User::paginate(3);
 
         $config = $this->config();
         $template = 'backend.user.index';
         return view ('backend.dashboard.layout', compact(
             'template',
             'config',
+            'users',
         ));
     }
 
--- public/library/library.js
+++ public/library/library.js
@@ -4,7 +4,14 @@
     "use strict";
     var HT = {};
     var $document = $(document)
+
+    HT.switchery = () => {
+        $('.js-switch').each(function() {
+            var switchery = new Switchery(this, {color: '#1AB394'});
+        })
+    }
+
     $document.ready(function(){
-        console.log(123);
-    })
+        HT.switchery();
+    });
 })(jQuery);
